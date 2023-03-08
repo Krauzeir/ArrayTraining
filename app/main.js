22567b284ec4edc714bc1eac3e46d5443037d5a2
Bruno Martin Costa Sacramento <bruno.sacramento@soluevo.com.br>
2023-03-08 13:50:24 -0300

    Day 2

--- app/main.js
+++ app/main.js
@@ -1,5 +1,4 @@
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
-const elementoParaInserirLivros = document.querySelector('#livros')
 let livros = []
 getBuscarLivrosDaAPI()
 
